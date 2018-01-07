51e8428144189db66194430df6166b8d3d93e98b
Aaron Lee <aaronhalstonlee@gmail.com>
2018-01-07 10:09:52 -0800

    day 11, completely started over and finished

--- webDevCourse/7-jQueryUI/codePlayer/js/main.js
+++ webDevCourse/7-jQueryUI/codePlayer/js/main.js
@@ -1,72 +1,72 @@
-var panels = 4;
-//html button/panel interaction
-$('#html-button').click(function(){
-    if ($('#html').is(':visible')){
-        $('#html').hide();
-        $('#html-button').css({'color': 'gray', 'text-decoration':'line-through'});
-        panels--;
-    } else if (!($('#html').is(':visible'))){
-        $('#html').show();
-        $('#html-button').css({'color': 'black', 'text-decoration':'none'});
-        panels++;
-        $('#hidden-p').hide();
-    }
-    $('.display').width(Math.floor(1000/panels));
-    if (panels < 1){
-        $('#hidden-p').show();
-    }
+//toggling panels
+var panels = 2;
+$('.panel').width(($(window).width()/panels)-2)
+
+$('#html').click(function(){
+  if ($('#html-panel').is(':visible')){
+    $('#html-panel').hide();
+    panels--;
+  } else if(!($('#html-panel').is(':visible'))){
+    $('#html-panel').show();
+    panels++
+  }
+  $('.panel').width( Math.floor($(window).width()/panels)-2);
 });
 
-//css button/panel interaction
-$('#css-button').click(function(){
-    if ($('#css').is(':visible')){
-        $('#css').hide();
-        $('#css-button').css({'color': 'gray', 'text-decoration':'line-through'});
-        panels--;
-    } else if (!($('#css').is(':visible'))){
-        $('#css').show();
-        $('#css-button').css({'color': 'black', 'text-decoration':'none'});
-        panels++;
-        $('#hidden-p').hide();
-    }
-    $('.display').width(Math.floor(1000/panels));
-    if (panels < 1){
-        $('#hidden-p').show();
-    }
+$('#css').click(function(){
+  if ($('#css-panel').is(':visible')){
+    $('#css-panel').hide();
+    panels--;
+  } else if(!($('#css-panel').is(':visible'))){
+    $('#css-panel').show();
+    panels++
+  }
+  $('.panel').width( Math.floor($(window).width()/panels)-2);
 });
 
-//js button/panel interaction
-$('#js-button').click(function(){
-    if ($('#js').is(':visible')){
-        $('#js').hide();
-        $('#js-button').css({'color': 'gray', 'text-decoration':'line-through'});
-        panels--;
-    } else if (!($('#js').is(':visible'))){
-        $('#js').show();
-        $('#js-button').css({'color': 'black', 'text-decoration':'none'});
-        panels++;
-        $('#hidden-p').hide();
-    }
-    $('.display').width(Math.floor(1000/panels));
-    if (panels < 1){
-        $('#hidden-p').show();
-    }
+$('#js').click(function(){
+  if ($('#js-panel').is(':visible')){
+    $('#js-panel').hide();
+    panels--;
+  } else if(!($('#js-panel').is(':visible'))){
+    $('#js-panel').show();
+    panels++
+  }
+  $('.panel').width( Math.floor($(window).width()/panels)-2);
 });
 
-//output button/panel interaction
-$('#output-button').click(function(){
-    if ($('#output').is(':visible')){
-        $('#output').hide();
-        $('#output-button').css({'color': 'gray', 'text-decoration':'line-through'});
-        panels--;
-    } else if (!($('#output').is(':visible'))){
-        $('#output').show();
-        $('#output-button').css({'color': 'black', 'text-decoration':'none'});
-        panels++;
-        $('#hidden-p').hide();
-    }
-    $('.display').width(Math.floor(1000/panels));
-    if (panels < 1){
-        $('#hidden-p').show();
-    }
+$('#output').click(function(){
+  if ($('iframe').is(':visible')){
+    $('iframe').hide();
+    panels--;
+  } else if(!($('#iframe').is(':visible'))){
+    $('iframe').show();
+    panels++
+  }
+  $('.panel').width( Math.floor($(window).width()/panels)-2);
+});
+//end of toggling panels
+// button functions
+$('.buttons').hover(function(){
+  $(this).addClass('highlight');  
+}, function(){
+  $(this).removeClass('highlight');
+});
+
+$('.buttons').click(function(){
+  $(this).toggleClass('select');
+  $(this).removeClass('highlight');
+});
+
+//active panel width
+
+//updating output pannel
+function updatePanel(){
+  $('iframe').contents().find('html').html('<html><head><style type="text/css">'+$('#css-panel').val()+'</style></head><body>'+$('#html-panel').val()+'</body></html>');
+  document.getElementById('output-panel').contentWindow.eval($('#js-panel').val());  
+}
+
+
+$('textarea').on('change keyup paste', function(){
+  updatePanel();
 });
